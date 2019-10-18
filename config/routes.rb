71df6f75f3015ec158dfd32ef20e28b742884f36
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-10-18 14:23:06 -0300

    Gera scaffold e devise de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
+  resources :usuarios
   resources :produtos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
