0228639ec0d68e024a09c93c70edc25097b84926
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-10-18 12:00:24 -0300

    Gera o scaffold de produto

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20191018143916) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "produtos", force: :cascade do |t|
+    t.integer "codigo"
+    t.string "nome"
+    t.string "marca"
+    t.integer "quantidade_estoque"
+    t.decimal "preco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "nome"
+    t.string "cpf"
+    t.string "endereco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
