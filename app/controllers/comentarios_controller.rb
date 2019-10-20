f855cd177fa875eaedf130b63c0ab110022bf359
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-10-20 13:00:01 -0300

    Cada usuario só pode alterar/remover seus comentarios
    Se for um admin, pode alterar/remover qualquer comentario

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -1,18 +1,7 @@
 class ComentariosController < ApplicationController
-  before_action :set_comentario, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin, only: [:edit, :update, :destroy]
-  before_action :authenticate_user, only: [:edit, :create, :update, :destroy]
-
-  # GET /comentarios/1
-  # GET /comentarios/1.json
-  def show
-  end
-
-  # GET /comentarios/new
-  def new
-    @produto = Produto.find(params[:produto_id])
-    @comentario = @produto
-  end
+  before_action :set_comentario, only: [:edit, :update, :destroy]
+  before_action :authenticate_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_logged_user, only: [:create]
 
   # GET /comentarios/1/edit
   def edit
@@ -26,8 +15,9 @@ class ComentariosController < ApplicationController
     @comentario.usuario = current_usuario
     respond_to do |format|
       if @comentario.save
-        format.html { redirect_to @produto, notice: 'Comentario was successfully created.' }
-        format.json { render :show, status: :created, location: @produto }
+        format.html { redirect_to produto_url(@produto), notice: 'Comentario was successfully created.' }
+        format.json { render :new, status: :created, location: @produto }
+        @comentario = @produto.comentarios.build
       else
         format.html { render :new }
         format.json { render json: @comentario.errors, status: :unprocessable_entity }
@@ -38,13 +28,15 @@ class ComentariosController < ApplicationController
   # PATCH/PUT /comentarios/1
   # PATCH/PUT /comentarios/1.json
   def update
-    respond_to do |format|
-      if @comentario.update(comentario_params)
-        format.html { redirect_to produto_path(@produto), notice: 'Comentario was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comentario }
-      else
-        format.html { render :edit }
-        format.json { render json: @comentario.errors, status: :unprocessable_entity }
+    if current_usuario.admin || @comentario.usuario == current_usuario
+      respond_to do |format|
+        if @comentario.update(comentario_params)
+          format.html { redirect_to produto_url(@produto), notice: 'Comentario was successfully updated.' }
+          format.json { render :new, status: :ok, location: @comentario }
+        else
+          format.html { render :edit }
+          format.json { render json: @comentario.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -52,34 +44,38 @@ class ComentariosController < ApplicationController
   # DELETE /comentarios/1
   # DELETE /comentarios/1.json
   def destroy
-    @comentario.destroy
-    respond_to do |format|
-      format.html { redirect_to produto_path(@produto), notice: 'Comentario was successfully destroyed.' }
-      format.json { head :no_content }
+    if current_usuario.admin || @comentario.usuario == current_usuario
+      @comentario.destroy
+      respond_to do |format|
+        format.html { redirect_to produto_url(@produto), notice: 'Comentario was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comentario
-      @produto = Produto.find(params[:produto_id])
-      @comentario = @produto.comentarios.find(params[:id])
-    end
 
-  def authenticate_admin
-    unless current_usuario.try(:admin?)
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comentario
+    @produto = Produto.find(params[:produto_id])
+    @comentario = @produto.comentarios.find(params[:id])
+  end
+
+  def authenticate_user
+    unless current_usuario.try(:admin?) || @comentario.usuario == current_usuario
       redirect_to produto_url
     end
   end
 
-  def authenticate_user
+  def authenticate_logged_user
     unless current_usuario
       redirect_to produto_url
     end
   end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comentario_params
-      params.require(:comentario).permit(:titulo, :texto)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comentario_params
+    params.require(:comentario).permit(:titulo, :texto)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :usuarios
   resources :usuarios
   resources :produtos do
-    resources :comentarios
+    resources :comentarios, only: [:update, :create, :edit, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
