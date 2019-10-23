ddc4892f77eb20c1d70f3b941aec375f9d6cb689
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-10-23 19:43:58 -0300

    Altera o ':with =>' para 'with:'

--- features/step_definitions/responder_comentarios_steps.rb
+++ features/step_definitions/responder_comentarios_steps.rb
@@ -1,31 +1,36 @@
 Given("Eu sou um administrador de nome {string} email {string} e senha {string}") do |nomeA, emailA, senhaA|
   visit 'usuarios/sign_up'
   expect(page).to have_text("Sign up")
-  fill_in 'usuario[email]', :with => emailA
-  fill_in 'usuario[password]', :with => senhaA
-  fill_in 'usuario[password_confirmation]', :with => senhaA
+  fill_in 'usuario[email]', with: emailA
+  fill_in 'usuario[password]', with: senhaA
+  fill_in 'usuario[password_confirmation]', with: senhaA
   check('usuario[admin]')
-  fill_in 'usuario[nome]', :with => nomeA
+  fill_in 'usuario[nome]', with: nomeA
   click_button 'SignUp'
 end
 
-And("Eu estou na pagina de visualizacao do produto de nome {string} codigo {string} marca {string} quantidade de estoque {int} e preco {float}") do |nomeP, codigoP, marcaP, qEstoqueP, precoP|
+And("Existe um produto de nome {string} codigo {string} marca {string} quantidade de estoque {int} e preco {float}") do |nomeP, codigoP, marcaP, qEstoqueP, precoP|
   visit '/produtos/new'
   expect(page).to have_text("New produto")
-  fill_in 'produto[codigo]', :with => codigoP
-  fill_in 'produto[nome]', :with => nomeP
-  fill_in 'produto[marca]', :with => marcaP
-  fill_in 'produto[quantidade_estoque]', :with => qEstoqueP
-  fill_in 'produto[preco]', :with => precoP
+  fill_in 'produto[codigo]', with: codigoP
+  fill_in 'produto[nome]', with: nomeP
+  fill_in 'produto[marca]', with: marcaP
+  fill_in 'produto[quantidade_estoque]', with: qEstoqueP
+  fill_in 'produto[preco]', with: precoP
   click_button 'cadastrar'
   visit '/produtos'
   click_link 'Show'
   expect(page).to have_text("Produto")
 end
 
+And("Eu estou na pagina do projeto de codigo {string}") do |codigo|
+  visit '/produtos'
+  click_link
+end
+
 And("O comentario de titulo {string} e texto {string} existe") do |clienteNome , textoComentario|
-  fill_in 'comentario[titulo]', :with => clienteNome
-  fill_in 'comentario[titulo]', :with => textoComentario
+  fill_in 'comentario[titulo]', with: clienteNome
+  fill_in 'comentario[titulo]', with: textoComentario
   click_button 'comentar_id'
 end
 
@@ -34,8 +39,8 @@ When("Eu clico na opcao responder comentario") do
 end
 
 And("Eu preencho o campo titulo {string} e o campo texto com {string}") do |nomeAdm, textoComentario|
-  fill_in 'comentario[titulo]', :with => nomeAdm
-  fill_in 'comentario[texto]', :with => textoComentario
+  fill_in 'comentario[titulo]', with: nomeAdm
+  fill_in 'comentario[texto]', with: textoComentario
 end
 
 And("Eu clico na opcao enviar resposta") do
@@ -84,8 +89,8 @@ Then("Eu vejo que a resposta de titulo {string} e texto {string} foi deletada") 
 end
 
 And("Eu preencho o campo titulo {string} e o campo texto com um texto vazio") do |tituloR|
-  fill_in 'comentario[titulo]', :with => tituloR
-  fill_in 'comentario[texto]', :with => ""
+  fill_in 'comentario[titulo]', with: tituloR
+  fill_in 'comentario[texto]', with: ""
 end
 
 Then("Eu vejo que a resposta de titulo {string} e texto vazio nao foi enviada") do |tituloR|
