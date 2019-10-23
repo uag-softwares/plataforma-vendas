347d54bc43d953a254c0e92d3c849bffbf371bb2
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-10-23 10:43:48 -0300

    Torna o auto-relacionamento entre comentarios opcional

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,5 +1,5 @@
 class Comentario < ApplicationRecord
-  belongs_to :comentavel, polymorphic: true
+  belongs_to :comentavel, polymorphic: true, optional: true
   belongs_to :usuario
   has_many :comentarios, as: :comentavel
 end
