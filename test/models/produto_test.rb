a0fdf7541647b573ebf583749270514692845cf0
FelipeBattisti <felipebattist@gmail.com>
2019-10-24 13:37:42 -0300

    test produto sem preco

--- test/models/produto_test.rb
+++ test/models/produto_test.rb
@@ -7,4 +7,9 @@ class ProdutoTest < ActiveSupport::TestCase
     assert_not produto.save
   end
 
+  test 'Nao salvar produto sem preco' do
+    produto = Produto.new codigo: '40028922',nome:'Blusa', marca: 'marcablusa', quantidade_estoque: 8
+    assert_not produto.save
+  end
+
 end
