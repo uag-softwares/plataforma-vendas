d5819093731d99083e8934c887d01432c9118fa6
FelipeBattisti <felipebattist@gmail.com>
2019-10-24 13:49:31 -0300

    test produto salvo corretamente

--- test/models/produto_test.rb
+++ test/models/produto_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class ProdutoTest < ActiveSupport::TestCase
 
+  test 'Garantir produto salvo' do
+    produto = Produto.new codigo: '40028922',nome:'Blusa', marca: 'marcablusa', quantidade_estoque: 8, preco: 34.90
+    assert produto.save
+  end
+
   test 'Nao salvar produto sem codigo' do
     produto = Produto.new nome:'Blusa', marca: 'marcablusa', quantidade_estoque: 8, preco: 34.90
     assert_not produto.save
@@ -23,7 +28,5 @@ class ProdutoTest < ActiveSupport::TestCase
     assert produto.save
     assert_not produto.update codigo: ' '
   end
-
-
 
 end
