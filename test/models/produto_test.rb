183179663bd8a8d2f883c0d5d7c3f04bbb2e6318
FelipeBattisti <felipebattist@gmail.com>
2019-10-24 13:48:03 -0300

    test produto update codigo vazio

--- test/models/produto_test.rb
+++ test/models/produto_test.rb
@@ -18,4 +18,12 @@ class ProdutoTest < ActiveSupport::TestCase
     assert_not produto.update preco: ' '
   end
 
+  test 'Nao editar produto deixando codigo vazio' do
+    produto = Produto.new codigo: '40028922',nome:'Blusa', marca: 'marcablusa', quantidade_estoque: 8, preco: 34.90
+    assert produto.save
+    assert_not produto.update codigo: ' '
+  end
+
+
+
 end
