24437b26cbda052424a05fd837f83f2a5749d0e5
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-10-28 18:39:13 -0300

    Renderiza os erros ao tentar salvar comentario invalido

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :usuarios
   resources :usuarios
   resources :produtos do
-    resources :comentarios
+    resources :comentarios, except: [:index, :show]
   end
   resources :comentarios do
-    resources :comentarios
+    resources :comentarios, except: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
