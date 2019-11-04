1c772daa4ce1bad6d0570f5bcf5509f4d9d7b9b4
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-04 08:55:29 -0300

    Adiciona modulo para buscar o pedido da sessao atual

---
+++ app/models/concerns/pedido_atual.rb
@@ -0,0 +1,10 @@
+module PedidoAtual
+    private
+
+    def set_pedido
+      @pedido = Pedido.find(session[:pedido_id])
+    rescue ActiveRecord::RecordNotFound
+      @pedido = Pedido.create
+      session[:pedido_id] = @pedido.id
+    end
+end
