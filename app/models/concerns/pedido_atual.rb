6d67c643c986ca219cada0e5c72af0efb6de914b
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-06 13:31:30 -0300

    So cria o pedido com um usuario logado

--- app/models/concerns/pedido_atual.rb
+++ app/models/concerns/pedido_atual.rb
@@ -2,16 +2,18 @@ module PedidoAtual
   private
 
   def set_pedido
-    @current = Pedido.find(session[:current_pedido_id])
-    if @current.status == 'efetuado'
-      new_pedido
+    if current_usuario
+      @current = Pedido.find(session[:current_pedido_id])
+      if @current.status == 'efetuado'
+        new_pedido
+      end
     end
   rescue ActiveRecord::RecordNotFound
     new_pedido
   end
 
   def new_pedido
-    @current = Pedido.create
+    @current = Pedido.create(usuario: current_usuario)
     session[:current_pedido_id] = @current.id
   end
 end
