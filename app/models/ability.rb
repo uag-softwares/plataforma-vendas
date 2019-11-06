11ad05e7c77179cbb7713dbb2922f8e7c2af9551
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-06 11:31:08 -0300

    Adiciona permissoes para o usuario

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     can :read, :all
     if user.present?
       can :manage, Comentario, usuario_id: user.id
+      can :manage, [Item, Pedido]
       if user.admin?
         can :manage, :all
       end
