a738af8b516afb6472396dd410b54cc986581483
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-10 19:26:40 -0300

    Remove espacos nos steps de receber pedido

--- features/step_definitions/receber_pedido_steps.rb
+++ features/step_definitions/receber_pedido_steps.rb
@@ -1,4 +1,4 @@
-And("O usuario de nome {string} efetuou o pedido de codigo {string} da compra do produto de codigo {int}") do |_nomeUser ,codigoPedido, codigoProduto|
+And("O usuario de nome {string} efetuou o pedido de codigo {string} da compra do produto de codigo {int}") do |_nomeUser, codigoPedido, codigoProduto|
   click_button 'adicionarAoCarrinho'
   expect(page).to have_text(codigoProduto)
   click_button 'finalizarPedidoUser'
@@ -6,12 +6,12 @@ And("O usuario de nome {string} efetuou o pedido de codigo {string} da compra do
   expect(page).to have_text(codigoProduto)
 end
 
-And ("Eu estou na pagina dos pedidos") do
+And("Eu estou na pagina dos pedidos") do
   visit '/pedidos'
   expect(page).to have_text('Pedidos')
 end
 
-When ("Eu clico na opcao visualizar pedido no pedido de codigo {string}") do |codigoPedido|
+When("Eu clico na opcao visualizar pedido no pedido de codigo {string}") do |codigoPedido|
   click_link 'ShowPedido'
   expect(page).to have_text(codigoPedido)
 end
@@ -24,32 +24,32 @@ When("Eu entro na pagina dos pedidos") do
   visit '/pedidos'
 end
 
-Then ('Eu consigo visualizar todos os pedidos efetuados') do
+Then('Eu consigo visualizar todos os pedidos efetuados') do
   expect(page).to have_text('Pedidos')
 end
 
-And ("Eu clico na opcao aceitar pedido") do
+And("Eu clico na opcao aceitar pedido") do
   click_button 'aceitarPedido'
   expect(page).to have_text('aprovado')
 end
 
-Then ("Eu vejo que o pedido de codigo {string} foi aceito com sucesso") do |_codigoPedido|
+Then("Eu vejo que o pedido de codigo {string} foi aceito com sucesso") do |_codigoPedido|
   click_button 'finalizarPedidoAdm'
   expect(page).to have_text('entregue')
 end
 
-And ("Eu clico na opcao recusar pedido") do
+And("Eu clico na opcao recusar pedido") do
   click_button 'cancelarPedido'
 end
 
-Then ("Eu vejo que o pedido de codigo {string} foi cancelado com sucesso") do |_codigoPedido|
+Then("Eu vejo que o pedido de codigo {string} foi cancelado com sucesso") do |_codigoPedido|
   expect(page).to have_text('cancelado')
 end
 
-And ("Eu clico na opcao reabrir pedido") do
+And("Eu clico na opcao reabrir pedido") do
   click_button 'refazerPedido'
 end
 
-Then ("Eu vejo que o pedido de codigo {string} foi reaberto com sucesso") do |_codigoPedido|
+Then("Eu vejo que o pedido de codigo {string} foi reaberto com sucesso") do |_codigoPedido|
   expect(page).to have_text('efetuado')
 end
