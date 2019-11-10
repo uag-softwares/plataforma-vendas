89723345712a1ea05eaa7032abd7993944f4c55c
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-10 00:52:57 -0300

    Atualiza testes do controller do item

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -9,23 +9,17 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     @usuario = usuarios(:usuario_dois)
   end
 
-  # test "should show item" do
-  #   sign_in @usuario
-  #   get pedido_item_path(@item.pedido, @item)
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   sign_in @usuario
-  #   get edit_pedido_item_url(@item.pedido, @item)
-  #   assert_response :success
-  # end
-  #
-  # test "should update item" do
-  #   sign_in @usuario
-  #   put pedido_item_url(@item.pedido, @item), params: { quantidade: 3 }
-  #   assert_redirected_to pedido_item_url(@item.pedido, @item)
-  # end
+  test "should get edit" do
+    sign_in @usuario
+    get edit_pedido_item_url(@item.pedido, @item)
+    assert_response :success
+  end
+
+  test "should update item" do
+    sign_in @usuario
+    put pedido_item_url(@item.pedido, @item), params: {item: {quantidade: 3}}
+    assert_redirected_to @item.pedido
+  end
 
   test "should destroy item" do
     sign_in @usuario
