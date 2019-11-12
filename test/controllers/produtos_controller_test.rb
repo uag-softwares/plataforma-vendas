558b4765cbee346730fbf7660c7efe25550d4426
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-12 15:15:08 -0300

    Adiciona testes de produto, no controller

--- test/controllers/produtos_controller_test.rb
+++ test/controllers/produtos_controller_test.rb
@@ -38,6 +38,18 @@ class ProdutosControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to produto_url(@produto)
   end
 
+  test "should not update produto" do
+    sign_in @admin
+    patch produto_url(@produto), params: {produto: {codigo: '', marca: @produto.marca, nome: @produto.nome, preco: @produto.preco, quantidade_estoque: @produto.quantidade_estoque}}
+    assert_response 422
+  end
+
+  test "should create produto" do
+    sign_in @admin
+    post produtos_url, params: {produto: {codigo: 123, marca: 'marca', nome: 'nome', preco: 15.5, quantidade_estoque: 5}}
+    assert_redirected_to Produto.last
+  end
+
   test "should destroy produto" do
     sign_in @admin
     assert_difference('Produto.count', -1) do
