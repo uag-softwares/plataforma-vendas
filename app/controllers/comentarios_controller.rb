ddbaf76a5aaca805369eb2e70cd55c41491d4c59
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-23 22:05:14 -0300

    Remove verificacoes desnecessarias

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -1,8 +1,8 @@
 class ComentariosController < ApplicationController
   before_action :set_comentavel
   before_action :set_comentario, only: [:edit, :update, :destroy]
-  before_action :authenticate_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_logged_user, only: [:create]
+  before_action :authenticate_usuario!
+  authorize_resource
 
   # GET /comentarios/1/edit
   def edit
@@ -42,13 +42,11 @@ class ComentariosController < ApplicationController
   # PATCH/PUT /comentarios/1
   # PATCH/PUT /comentarios/1.json
   def update
-    if current_user.admin || @comentario.usuario == current_user
-      respond_to do |format|
-        if @comentario.update(comentario_params)
-          response_successfully(format, produto, 'Comentario was successfully updated.', :show, :ok)
-        else
-          response_unsuccessfully(format, :edit, @comentario, :unprocessable_entity)
-        end
+    respond_to do |format|
+      if @comentario.update(comentario_params)
+        response_successfully(format, produto, 'Comentario was successfully updated.', :show, :ok)
+      else
+        response_unsuccessfully(format, :edit, @comentario, :unprocessable_entity)
       end
     end
   end
@@ -56,11 +54,9 @@ class ComentariosController < ApplicationController
   # DELETE /comentarios/1
   # DELETE /comentarios/1.json
   def destroy
-    if current_user.admin || @comentario.usuario == current_user
-      @comentario.destroy
-      respond_to do |format|
-        response_successfully(format, produto, 'Comentario was successfully destroyed.', :show, :no_content)
-      end
+    @comentario.destroy
+    respond_to do |format|
+      response_successfully(format, produto, 'Comentario was successfully destroyed.', :show, :no_content)
     end
   end
 
@@ -77,18 +73,6 @@ class ComentariosController < ApplicationController
       @comentavel = Comentario.find(params[:comentario_id])
     elsif params[:produto_id]
       @comentavel = Produto.find(params[:produto_id])
-    end
-  end
-
-  def authenticate_user
-    unless current_user.try(:admin?) || @comentario.usuario == current_user
-      redirect_to produto_url
-    end
-  end
-
-  def authenticate_logged_user
-    unless current_user
-      redirect_to produto_url
     end
   end
 
