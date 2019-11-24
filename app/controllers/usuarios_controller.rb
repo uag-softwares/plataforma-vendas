80a24c6eed894427d9e7dfaf45942afd0b1376e9
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2019-11-24 16:40:00 -0300

    Remove opcao do usuario escolher ser admin quando se cadastrar

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -2,10 +2,10 @@ class UsuariosController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:usuario).permit(:nome, :admin, :email, :password, :password_confirmation)
+    params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:usuario).permit(:nome, :admin, :email, :password, :password_confirmation, :current_password)
+    params.require(:usuario).permit(:nome, :email, :password, :password_confirmation, :current_password)
   end
 end
